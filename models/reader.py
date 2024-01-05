81f5b8d54c6810ae279b4d413012d9ba87d10596
ChenXiangYpily1234 <126395776+ChenXiangYpily1234@users.noreply.github.com>
2024-01-05 16:32:28 +0800

    增加phone,删除sex

--- models/reader.py
+++ models/reader.py
@@ -8,7 +8,7 @@ class Reader(db.Model):
     name = db.Column(db.String(100), nullable=False, default=None)
     password = db.Column(db.String(20), nullable=False, default='123456')
     email = db.Column(db.String(100), nullable=False, unique=True, default=None)
-    sex = db.Column(db.String(1), nullable=False, default=None)
+    phone= db.Column(db.String(20), nullable=False, default=None)
     fine = db.Column(db.DECIMAL(6, 2), nullable=False, default=0.00)
     borrow_num = db.Column(db.Integer, nullable=False, default=0,
                            comment='一名读者最多只能借阅十本图书，且每本图书最多只能借两个月')
